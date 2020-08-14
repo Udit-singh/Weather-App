a6adf1f07c998592ebc790f519a494716f059298
Udit singh <Udit-singh@users.noreply.github.com>
2020-08-14 15:28:13 +0530

    Api implementation

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() {
+    return _LoadingScreenState();
+  }
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
